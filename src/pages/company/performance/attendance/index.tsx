631c4dd20a23a98a436aa1ebac1488b81868eb88
HirenBhuva <hirenbhuva1722@gmail.com>
2026-07-30 10:42:35 +0530

    Build issue resolved

--- src/pages/company/performance/attendance/index.tsx
+++ src/pages/company/performance/attendance/index.tsx
@@ -1,5 +1,5 @@
 import EmptyPlaceholder from "../../../../components/common/empty-paceholder";
-import Attendance from "../../../../components/company/performance/attendance";
+// import Attendance from "../../../../components/company/performance/attendance";
 
 export default function AttendancesPage(){
     return <EmptyPlaceholder title="Under Development" description="this page is currently under development."/>
